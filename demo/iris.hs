b4bd4674f9f31514439013bdbb73144204f91d46
Peter Hurford <peter@peterhurford.com>
2015-10-10 19:16:51 -0500

    import qualified DMatrix

--- demo/iris.hs
+++ demo/iris.hs
@@ -1,25 +1,25 @@
 module Main where
 import qualified Text.CSV as CSV
 import qualified Data.List as L
-import DMatrix
+import qualified DMatrix as D
 
 handleError csv = putStrLn "error parsing"
 
 doWork :: [[String]] -> IO ()
 doWork csv = do
   print "Petal.Length"
-  print $ map strToFloat $ row "Petal.Length" csv
-  print $ (!!1) $ headers csv
-  print $ map strToFloat $ index 1 csv
+  print $ map D.strToFloat $ D.row "Petal.Length" csv
+  print $ (!!1) $ D.headers csv
+  print $ map D.strToFloat $ D.index 1 csv
   print $ (!!2) $ head csv
-  print $ map strToFloat $ index 2 csv
+  print $ map D.strToFloat $ D.index 2 csv
   print $ (!!3) $ head csv
-  print $ map strToFloat $ index 3 csv
+  print $ map D.strToFloat $ D.index 3 csv
   print $ (!!4) $ head csv
-  print $ map strToFloat $ index 4 csv
+  print $ map D.strToFloat $ D.index 4 csv
   print $ (!!5) $ head csv
-  print $ L.nub $ index 5 csv
-  print $ factorize $ index 5 csv
+  print $ L.nub $ D.index 5 csv
+  print $ D.factorize $ D.index 5 csv
 
 main :: IO ()
 main = do
