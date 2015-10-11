3cefbad95834b943321265bc4c588ad30793b6ec
Peter Hurford <peter@peterhurford.com>
2015-10-10 22:40:16 -0500

    Dummyize dataframes (they just need to be recombined)

--- demo/iris.hs
+++ demo/iris.hs
@@ -7,25 +7,28 @@ handleError csv = putStrLn "error parsing"
 
 doWork :: [[String]] -> IO ()
 doWork csv = do
+  let df = D.DataFrame { D.headers = head csv, D.body = D.listBody csv }
   print "ID"
-  print $ map D.strToFloat $ D.row "ID" csv
+  print $ map D.strToFloat $ D.column "ID" df
   print "Petal.Length"
-  print $ map D.strToFloat $ D.row "Petal.Length" csv
-  print $ (!!1) $ D.headers csv
-  print $ map D.strToFloat $ D.index 1 csv
-  print $ (!!2) $ D.headers csv
-  print $ map D.strToFloat $ D.index 2 csv
-  print $ (!!3) $ D.headers csv
-  print $ map D.strToFloat $ D.index 3 csv
-  print $ (!!4) $ D.headers csv
-  print $ map D.strToFloat $ D.index 4 csv
-  print $ (!!5) $ D.headers csv
-  print $ L.nub $ D.index 5 csv
-  print $ D.factorize $ D.index 5 csv
+  print $ map D.strToFloat $ D.column "Petal.Length" df
+  print $ (!!1) $ D.headers df
+  print $ map D.strToFloat $ D.index 1 df
+  print $ (!!2) $ D.headers df
+  print $ map D.strToFloat $ D.index 2 df
+  print $ (!!3) $ D.headers df
+  print $ map D.strToFloat $ D.index 3 df
+  print $ (!!4) $ D.headers df
+  print $ map D.strToFloat $ D.index 4 df
+  print $ (!!5) $ D.headers df
+  print $ D.unique $ D.index 5 df
+  print $ D.factorize $ D.index 5 df
   print "The Whole Thing (deparsed)"
-  print $ L.transpose $ D.body csv
+  print $ L.transpose $ D.body df
   print "The Whole Thing (parsed)"
-  print $ D.toDMatrix csv
+  print $ D.toDMatrix df
+  print "The Whole Thing (dummies)"
+  print $ D.dummyDf $ L.transpose $ D.body df
 
 main :: IO ()
 main = do
--- src/DMatrix.hs
+++ src/DMatrix.hs
@@ -3,42 +3,97 @@ import qualified Data.List as L
 import qualified Text.Read as R
 import qualified Data.Maybe as D
 
+data DataFrame = DataFrame {
+  headers :: [String], body :: [[String]] } deriving (Show)
+
+type DMatrix = [[Float]]
+
+unique = L.nub
+
 strToFloat :: String -> Float
 strToFloat s = case R.readMaybe s :: Maybe Float of
   Just i -> realToFrac i :: Float
   Nothing -> 9.9
 
-headers :: [[String]] -> [String]
-headers = head
 
-body :: [[String]] -> [[String]]
-body xs = tail $ init xs
+listBody :: [[String]] -> [[String]]
+listBody xs = tail $ init xs
 
-index :: Int -> [[String]] -> [String]
-index i xs = map (!! i) (body xs)
 
-indexByRow :: String -> [[String]] -> Maybe Int
-indexByRow row xs = L.elemIndex row (headers xs) 
+index :: Int -> DataFrame -> [String]
+index i df = map (!! i) (body df)
 
-row :: String -> [[String]] -> [String]
-row r xs = case indexByRow r xs of
-  Just i -> index i xs
+
+indexByColumn :: String -> DataFrame -> Maybe Int
+indexByColumn column df = L.elemIndex column (headers df) 
+
+
+column :: String -> DataFrame -> [String]
+column c df = case indexByColumn c df of
+  Just i -> index i df
   Nothing -> []
+
 
 factorize :: [String] -> [Float]
 factorize xs = map fn xs where
-  fn x = case L.elemIndex x (L.nub xs) of
+  fn x = case L.elemIndex x (unique xs) of
     Just i -> realToFrac i :: Float
     Nothing -> 9.9
 
-rowIsFactors :: [String] -> Bool
-rowIsFactors r = and $ map D.isNothing $
-  map (R.readMaybe :: String -> Maybe Float) r
 
-convertRow :: [String] -> [Float]
-convertRow r = case rowIsFactors r of
-  False -> map strToFloat r
-  True -> factorize r
+dummyLists :: [String] -> [[String]]
+dummyLists column = map fn (unique column) where
+  fn x = [ if j == x then "1" else "0" | j <- column ]
 
-toDMatrix :: [[String]] -> [[Float]]
-toDMatrix xs = map convertRow (L.transpose $ body xs)
+
+processColumn :: [String] -> [[String]]
+processColumn c = case columnIsFactors c of
+  False -> [c] --map strToFloat c
+  True -> dummyLists c
+
+
+dummyDf :: [[String]] -> [[[String]]]
+dummyDf []     = []
+dummyDf (x:xs) = (processColumn x) : (dummyDf xs)
+
+
+-- class Mungeable [a] where
+--   munge1 :: [a] -> [[b]]
+
+-- instance Mungeable [String] where
+--   munge1 :: [String] -> [[b]]
+
+data Column = Float [Float] | Character [String]
+type Mungestep = Column -> [Column]
+
+munge :: [Mungestep] -> DataFrame -> DataFrame
+munge []     df = df
+munge (x:xs) df = munge xs $ munge1 x $ body df
+
+munge1 :: Mungestep -> DataFrame -> DataFrame
+munge1 m df = foldl (++) (m df) []
+
+
+columnIsFactors :: [String] -> Bool
+columnIsFactors c = or $ map D.isNothing $
+  map (R.readMaybe :: String -> Maybe Float) c
+
+
+seqAlong :: [a] -> [Int]
+seqAlong xs = [0..(length xs -1)]
+
+
+--TODO: I ended up not needing this, but I don't want to get rid of it.
+which :: [Bool] -> [Int]
+which xs = filter (\x -> x /= -1)
+  [ if (xs !! j) == True then j else -1 | j <- seqAlong xs ]
+
+
+convertColumn :: [String] -> [Float]
+convertColumn c = case columnIsFactors c of
+  False -> map strToFloat c
+  True -> factorize c
+
+
+toDMatrix :: DataFrame -> DMatrix
+toDMatrix xs = map convertColumn (L.transpose $ body xs)
